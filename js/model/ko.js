a99e018c9ce806fe61d92fa8027081c411cc4f6f
prunk1al <prunk1al@gmail.com>
2015-07-31 17:55:50 +0200

    player added

--- js/model/ko.js
+++ js/model/ko.js
@@ -57,7 +57,6 @@ var viewModel=function(){
         this.init=function(){
             new self.topMenu();
             self.setPlayList();
-            self.player.init();
         };
 
        
@@ -124,34 +123,78 @@ var viewModel=function(){
         };
 
 
-        this.player={
-
-            render: function(){
-                
-                var tag = document.createElement('script');
-                tag.src = "http://www.youtube.com/iframe_api";
-                var firstScriptTag = document.getElementsByTagName('script')[0];
-                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
-
-            },
-
-                    
-
-            loadVideo: function(){
-                player.loadVideoById(this.ytid(), 0, "large")
-                self.currentVideo(this);
-                self.trackList.splice(self.trackList.indexOf(this),1);
-            },
-
-            init: function(){
-                this.render();
-            }    
-        };
+        this.changeCurrentVideo=function(video){
+            console.log(this)
+            window.player.loadVideoById(video.ytid(), 0,"large")
+            self.currentVideo(video.ytid())
+            self.trackList.splice(self.trackList.indexOf(this),1)
+        }
 
 this.init()
 }
+
+
+ko.bindingHandlers['player'] = {
+        init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+            // Check if global script and function is declared.
+            if ( !document.getElementById('playerScript') ) {
+                // Create script
+                var tag = document.createElement('script');
+                tag.src = "https://www.youtube.com/iframe_api";
+                var playerDiv = document.getElementById('player');
+                var firstScriptTag = document.getElementsByTagName('script')[0];
+                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+                // Create global function that their API calls back
+                window.playerReady = ko.observable(false);
+                window.onYouTubeIframeAPIReady = function() {
+                    window.playerReady(true);
+                };
+            }
+            console.log(valueAccessor)
+        },
+        update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+
+            var value = valueAccessor(),
+                id = viewModel.currentVideo(),
+                height = ko.unwrap(value.height) || '250',
+                width = ko.unwrap(value.width) || '444'
+            ;
+
+            var view=ko.bindingHandlers.foreach.update(element, valueAccessor,
+            allBindings, viewModel, bindingContext);
+
+            console.log(view)
+
+            if ( !window.playerReady() ) {
+                // YT hasn't invoked global callback.  Subscribe to update
+                var subscription;
+                subscription = window.playerReady.subscribe( function(newValue) {
+                     if ( newValue ) {
+                         subscription.dispose();
+                         // Just get this binding to fire again
+                         value.id.notifySubscribers(value.id());
+                     }
+                });
+            } else {
+                if ( !value.id()) {
+                
+                window.player = new YT.Player( element, {
+                    height: height,
+                    width: width,
+                });
+                  }
+                else{
+                var player = new YT.Player( element, {
+                    height: height,
+                    width: width,
+                    videoId: id 
+                });
+            }
+            }
+        },
+    }
+    
 
 
 ko.applyBindings(new viewModel())
